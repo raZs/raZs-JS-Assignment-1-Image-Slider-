261ec6c272cf1adbd2f04af2eed094f621279123
razs <razeshpuri@gmail.com>
2021-05-11 11:49:02 +0545

    first intit

--- js/carosel.js
+++ js/carosel.js
@@ -31,25 +31,25 @@ nextBtn.addEventListener('click' ,()=>{
 });
 
 
-// carouselSlide.addEventListener('transitioned',()=> {
+carouselSlide.addEventListener('transitioned',()=> {
 
-//     if (carouselImages[counter].id ==='lastClone'){
-//         carouselSlide.style.transition ="none";
-//         counter = carouselImages.length - 2 ; 
-//         carouselSlide.style.transform = 'translateX ( ' + (-size*counter) +' px ) ';
+    if (carouselImages[counter].id ==='lastClone'){
+        carouselSlide.style.transition ="none";
+        counter = carouselImages.length - 2 ; 
+        carouselSlide.style.transform = 'translateX ( ' + (-size*counter) +' px ) ';
 
-//     }
+    }
 
 
 
-//     if (carouselImages[counter].id ==='firstClone'){
-//         carouselSlide.style.transition ="none";
-//         counter = carouselImages.length - counter;
-//         carouselSlide.style.transform = 'translateX(' + (-size*counter) +'px)';
+    if (carouselImages[counter].id ==='firstClone'){
+        carouselSlide.style.transition ="none";
+        counter = carouselImages.length - counter;
+        carouselSlide.style.transform = 'translateX(' + (-size*counter) +'px)';
         
-//     }
+    }
     
-// });
+});
 
 
 
