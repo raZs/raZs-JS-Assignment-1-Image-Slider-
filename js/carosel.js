c0aa7c3bb7c309737efd7738caad9aae390b2370
razs <razeshpuri@gmail.com>
2021-05-11 10:55:48 +0545

    first intit

---
+++ js/carosel.js
@@ -0,0 +1,60 @@
+const carouselSlide = document.querySelector('.carousel-slide');
+const carouselImages = document.querySelectorAll('.carousel-slide img');
+
+
+//Buttons
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
+
+//Counter
+let counter = 1;
+const size = carouselImages[0].clientWidth;
+
+carouselSlide.style.transform ='translatex(' +(-size * counter) + 'px)';
+
+//Button List
+
+prevBtn.addEventListener('click' ,()=>{
+   // if (counter<=0) return;
+    carouselSlide.style.transition = "transform 0.s ease-in-out";
+    counter--
+    carouselSlide.style.transform ='translatex(' +(-size * counter) + 'px)';
+
+});
+
+nextBtn.addEventListener('click' ,()=>{
+   // if(counter>= carouselImages -1) return;
+    carouselSlide.style.transition = "transform 0.1s ease-in-out";
+    counter ++
+    carouselSlide.style.transform ='translatex ( ' +(-size * counter) + 'px ) ';
+
+});
+
+
+// carouselSlide.addEventListener('transitioned',()=> {
+
+//     if (carouselImages[counter].id ==='lastClone'){
+//         carouselSlide.style.transition ="none";
+//         counter = carouselImages.length - 2 ; 
+//         carouselSlide.style.transform = 'translateX ( ' + (-size*counter) +' px ) ';
+
+//     }
+
+
+
+//     if (carouselImages[counter].id ==='firstClone'){
+//         carouselSlide.style.transition ="none";
+//         counter = carouselImages.length - counter;
+//         carouselSlide.style.transform = 'translateX(' + (-size*counter) +'px)';
+        
+//     }
+    
+// });
+
+
+
+
+
+
+
+
